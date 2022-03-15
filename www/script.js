bc9fd61860e83f6f6fa071e4d77fc29dc6d3d510
reina jones <reinajones111@gmail.com>
2022-03-14 18:21:13 -0600

    fsh

--- www/script.js
+++ www/script.js
@@ -1,7 +1,12 @@
-const url = "https://inspiration.goprogram.ai"
+const url = "https://api.kanye.rest"
   fetch(url)
   .then(function(response) {
     return response.json();
   }).then(function(json) {
     console.log(json);
+    show(json.quote);
   });
+
+  function show(quote) {
+     document.getElementById("quote").innerText=`"${quote}"`;
+  }
